c114ab175221452c72d18456013fd2cb513fff8f
Riyansh Kamboj <riyanshkamboj100@gmail.com>
2025-03-12 14:07:32 +0530

    Final commit

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-from setuptools import find_packages,setup
 from typing import List
+from setuptools import find_packages, setup
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT = "-e ."
+
 def get_requirements(file_path:str)->List[str]:
+    
     '''
     this function will return the list of requirements
     '''
+
     requirements=[]
-    with open(file_path)as file_obj:
+    with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        [req.replace("\n","")for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-
-
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
 
 setup(
+
 name='mlproject',
 version='0.0.1',
-author='Riyansh',
-author_email='riyanshkamboj100@gmail.com',
+author='Dhruv',
+author_email='dhruv63tyagi@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 )
-
--- src/utils.py
+++ src/utils.py
@@ -1,65 +1,62 @@
 import os 
-import sys
+import sys 
 
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import dill
-import pickle
+from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from src.exception import CustomException
-from src.logger import logging
-
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj, file_obj)
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-
-
-def evaluate_model(X_train,y_train,X_test,y_test,models,param):
+    
+def evaluate_models(X_train, y_train,X_test, y_test,models,param):
     try:
-        report={}
+        report = {}
         for i in range(len(list(models))):
-            model=list(models.values())[i]
-            model_name=list(models.keys())[i]
-            print("Model Name: ",model_name)
-            logging.info(f"Model Name: {model_name}")
+            model = list(models.values())[i]
             para=param[list(models.keys())[i]]
-           
-            gs=GridSearchCV(model,para, cv=5)
+
+            gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
-            print(f"Best Params for{model_name}: ",gs.best_params_)
-            logging.info(f"Best params for {model_name} are {gs.best_params_}")
+
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-
-            y_train_pred=model.predict(X_train)
-
-            y_test_pred=model.predict(X_test)
-
-            train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
-            print("Train model R2_score: ",train_model_score,"  ", "Test Model R2_score",test_model_score)
-            logging.info(f"Train model R2_score: {train_model_score} Test Model R2_score {test_model_score}")
-
-            report[list(models.keys())[i]]=test_model_score
         
-        return report
+
+            # model.fit(X_train, y_train)  # train model
 
 
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report 
+    
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
 
 def load_object(file_path):
     try:
-        with open(file_path,"rb") as file_obj:
-            return pickle.load(file_obj)
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
